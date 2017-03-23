33adbcf89abaf6bf7b4496d9317e26b6eacf1678
john <john@JOHNVANDENBROEK>
2017-03-23 18:03:11 +0100

    2 tests completed

--- Herkansing6B/test_unit.py
+++ Herkansing6B/test_unit.py
@@ -1,5 +1,7 @@
 import unittest
-from testfunctions import *
+from test_functions import *
+from Herkansing6B import *
+from flask import Flask 
 
 class Test_Model_Answers(unittest.TestCase):
 	def setUp(self):
@@ -14,18 +16,18 @@ class Test_Model_Answers(unittest.TestCase):
 		response = self.app.post('/easyresult', data = dict(ID="3", verb="hallokidokie"), follow_redirects=True)
 		self.assertTrue("Doesn't exist", response.data)
 
-class Test_Model_Result(unittest.Testcase):
+class Test_Model_Result(unittest.TestCase):
 	def setUp(self):
 		self.app = app.test_client()
 		self.app.testing = True
 
 	def test_model_result_equal(self):
-		response = self.app.post('/easyresult', data = dict(answer="smoke"), follow_redirects=True)
+		response = self.app.post('/easyresult', data = dict(ID="1", answer="to smoke"), follow_redirects=True)
 		self.assertIn("Correct, Score increased by 10", response.data) 
 
 	def test_model_result_diff(self):
-		response = self.app.post('/easyresult', data = dict(answer="smokey"), follow_redirects=True)
-		self.assertIn("False, Score decreased by 10", response.data) 
+		response = self.app.post('/easyresult', data = dict(ID="1", answer="smokey"), follow_redirects=True)
+		self.assertisNot("False, Score decreased by 10", response.data) 
 
 if __name__ == '__main__':
 	unittest.main()
